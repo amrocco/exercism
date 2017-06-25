7a65991f5fa1365ffb1b05073a0598f45f511b4a
amrocco <arocco89@gmail.com>
2017-06-25 15:44:45 -0400

    leap: second iteration

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -4,8 +4,5 @@ const testVersion = 3
 
 //IsLeapYear is a function that determines if a calendar year is a leap year or not
 func IsLeapYear(year int) bool {
-	if year%4 == 0 && !(year%100 == 0 && year%400 != 0) {
-		return true
-	}
-	return false
+	return year%4 == 0 && !(year%100 == 0 && year%400 != 0)
 }
